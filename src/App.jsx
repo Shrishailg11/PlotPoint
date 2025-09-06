495f841f8aa42676df710c5b0af9a14c9854887d
Shrishail <shrishailgolappanavar11@gmail.com>
2025-09-06 14:08:09 +0530

    Pages and Routes Created

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,23 @@
-import { useState } from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
+import SignIn from './pages/SignIn'
+import SignUp from './pages/SignUp'
+import SignUp from './pages/SignUp'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-        <h1 className='bg-red-100'>app</h1>
+        <BrowserRouter>
+          <Routes>
+             <Route path='/' element={<Home />}></Route>
+             <Route path='/sign-in' element={<SignIn />} />
+             <Route path='/sign-up' element={ <SignUp />}></Route>
+             <Route path='profile' element={<Profile />} />
+             <Route path='/about' element={<About />} />
+          </Routes>
+        </BrowserRouter>
     </>
   )
 }
