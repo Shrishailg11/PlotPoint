c34f5784a07b6e05cd071893fb767a77ce5486af
Shrishailg11 <shrishailgolappanavar11@gmail.com>
2025-09-13 13:19:07 +0530

    About page is done

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
 
-function About() {
-    return <>
-    <h1>About</h1>
-    </>
+export default function About() {
+  return (
+    <div className='py-20 px-4 max-w-6xl mx-auto'>
+      <h1 className='text-3xl font-bold mb-4 text-slate-800'>About PlotPoint</h1>
+      <p className='mb-4 text-slate-700'> At PlotPoint, we don’t just sell properties — we build lasting relationships. With years of expertise in buying, selling, and renting across the most sought-after neighborhoods, our team is committed to making your journey smooth, transparent, and rewarding. Whether you’re a first-time buyer, an investor, or a family searching for your dream home, you can count on us for expert guidance every step of the way.</p>
+      <p className='mb-4 text-slate-700'>
+      Your home is more than just walls and a roof — it’s where life happens. That’s why Sahand Estate goes beyond listings to truly understand your lifestyle, preferences, and goals. From stylish city apartments to spacious family homes, we connect you with spaces that feel just right. Renting, buying, or selling — whatever your journey, we make it simple, stress-free, and perfectly tailored to you.
+      </p>
+      <p className='mb-4 text-slate-700'>At PlotPoint, we believe finding the right property should feel as extraordinary as the home itself. Our expert team pairs in-depth market knowledge with personalized service to help you unlock exclusive opportunities in the finest neighborhoods. Whether you’re upgrading to your forever home, exploring investment options, or renting with ease, we’re here to make your real estate journey seamless, exciting, and truly rewarding.</p>
+    </div>
+  )
 }
-
-export default About
